2a774fb92a0f0103e2f15e08e9a6d67cf4b8227d
Barogthor <f.thery3103@gmail.com>
2022-03-14 19:46:07 +0100

    add menu to breakout

--- examples/breakout/menu.rs
+++ examples/breakout/menu.rs
@@ -1,42 +1,72 @@
 use bevy::prelude::*;
-
+use crate::AppState;
+use bevy::app::AppExit;
 
 const NORMAL_BUTTON: Color = Color::rgb(0.15, 0.15, 0.15);
 const HOVERED_BUTTON: Color = Color::rgb(0.45, 0.45, 0.45);
 const PRESSED_BUTTON: Color = Color::rgb(0.35, 0.75, 0.35);
 
-pub fn add_menu_stage(mut commands: Commands, asset_server: Res<AssetServer>) {
-    setup(commands, asset_server);
+
+#[derive(Component)]
+pub struct MainMenuUI;
+
+#[derive(Component)]
+pub enum MenuAction {
+    Play,
+    Exit
+}
+
+pub fn menu_quit_app(mut app_exit_events: EventWriter<AppExit> ) {
+    app_exit_events.send(AppExit);
+}
+
+pub fn close_menu(
+    mut commands: Commands,
+    query: Query<Entity, With<MainMenuUI>>,
+) {
+    for entity in query.iter() {
+        // despawn the entity and its children
+        commands.entity(entity).despawn_recursive();
+    }
 }
 
 
-pub fn to_game_system(
+pub fn button_system(
     mut interaction_query: Query<
-        (&Interaction, &mut UiColor, &Children),
+        (&Interaction, &mut UiColor, &MenuAction),
         (Changed<Interaction>, With<Button>),
     >,
-    mut text_query: Query<&mut Text>,
+    mut app_state: ResMut<State<AppState>>
 ) {
-    for (interaction, mut color, children) in interaction_query.iter_mut() {
-        let mut text = text_query.get_mut(children[0]).unwrap();
-        match *interaction {
-            Interaction::Clicked => {
-                text.sections[0].value = "Press".to_string();
-                // *color = PRESSED_BUTTON.into();
+    for (interaction, mut color, action) in interaction_query.iter_mut() {
+        match (*interaction, action) {
+            (Interaction::Clicked, MenuAction::Play) => {
+                println!("play {:?}", app_state.current());
+                *color = PRESSED_BUTTON.into();
+                app_state.set(AppState::InGame);
             }
-            Interaction::Hovered => {
-                text.sections[0].value = "Hover".to_string();
+            (Interaction::Hovered, MenuAction::Play) => {
                 *color = HOVERED_BUTTON.into();
             }
-            Interaction::None => {
-                text.sections[0].value = "Button".to_string();
+            (Interaction::None, MenuAction::Play) => {
+                *color = NORMAL_BUTTON.into();
+            }
+            (Interaction::Clicked, MenuAction::Exit) => {
+                println!("exit {:?}", app_state.current());
+                *color = PRESSED_BUTTON.into();
+                app_state.set(AppState::Quit);
+            }
+            (Interaction::Hovered, MenuAction::Exit) => {
+                *color = HOVERED_BUTTON.into();
+            }
+            (Interaction::None, MenuAction::Exit) => {
                 *color = NORMAL_BUTTON.into();
             }
         }
     }
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+pub fn setup_menu(mut commands: Commands, asset_server: Res<AssetServer>) {
     // ui camera
     commands.spawn_bundle(UiCameraBundle::default());
     commands
@@ -54,10 +84,43 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             color: NORMAL_BUTTON.into(),
             ..Default::default()
         })
+        .insert(MenuAction::Play)
+        .insert(MainMenuUI)
         .with_children(|parent| {
             parent.spawn_bundle(TextBundle {
                 text: Text::with_section(
-                    "Button",
+                    "Play",
+                    TextStyle {
+                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
+                        font_size: 40.0,
+                        color: Color::rgb(0.9, 0.9, 0.9),
+                    },
+                    Default::default(),
+                ),
+                ..Default::default()
+            });
+        });
+    commands
+        .spawn_bundle(ButtonBundle {
+            style: Style {
+                size: Size::new(Val::Px(150.0), Val::Px(65.0)),
+                // center button
+                margin: Rect::all(Val::Auto),
+                // horizontally center child text
+                justify_content: JustifyContent::Center,
+                // vertically center child text
+                align_items: AlignItems::Center,
+                ..Default::default()
+            },
+            color: NORMAL_BUTTON.into(),
+            ..Default::default()
+        })
+        .insert(MenuAction::Exit)
+        .insert(MainMenuUI)
+        .with_children(|parent| {
+            parent.spawn_bundle(TextBundle {
+                text: Text::with_section(
+                    "Exit",
                     TextStyle {
                         font: asset_server.load("fonts/FiraSans-Bold.ttf"),
                         font_size: 40.0,
